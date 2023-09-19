7d7c4a134e5664400edf3a993cf14d795ccad60c
Bug Smith <salttyyy@gmail.com>
2023-09-19 00:00:04 +0000

    submit handler in progress #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const $photoURL = document.querySelector('#photo-url');
 const $photo = document.querySelector('.img');
+const $form = document.querySelector('form');
 
 $photoURL.addEventListener('input', function (event) {
   $photo.setAttribute('src', event.target.value);
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const obj = {
+    title: $form.elements.title.value,
+    url: $form.elements.photourl.value,
+    notes: $form.elements.notes.value,
+  };
+
+  obj.entryID = data.nextEntryId;
+  // nextEntryId++
+
+  for (let i = 0; i < data.length; i++) {
+    data.entries = obj.push(obj[i]);
+  }
 });
