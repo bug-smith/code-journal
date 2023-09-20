307bfb79de8ef10ac1cb5ea0fd5220cec75cb1c4
Bug Smith <salttyyy@gmail.com>
2023-09-20 00:11:42 +0000

    completed <a> and viewSwap #2

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ const $photoURL = document.querySelector('#photo-url');
 const $photo = document.querySelector('.img');
 const $form = document.querySelector('form');
 
+
+// assigns image URL to produce the IMAGE
+
 $photoURL.addEventListener('input', function (event) {
   $photo.setAttribute('src', event.target.value);
 });
+
+// assigns entry to new obj stored in data
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -23,3 +28,85 @@ $form.addEventListener('submit', function (event) {
 
   $form.reset();
 });
+
+
+// assigns DOM tree
+
+function renderEntry (entry) {
+
+  // <li class='row'>
+  // <div class='column-half'>
+  //   <img src='$photoURL' alt="image"/>
+  // </div>
+  // <div class='column-half'>
+  //   <h4>A newer image</h4>
+  //   <p>A very nice lake</p>
+  // </div>
+  // </li>
+
+  const $li = document.createElement('li')
+  $li.setAttribute('class', 'row')
+
+  const $divHalfOne = document.createElement('div')
+  $divHalfOne.setAttribute('class', 'column-half')
+
+  const $img = document.createElement('img')
+  $img.setAttribute('src', entry.url)
+
+  const $divHalfTwo = document.createElement('div')
+  $divHalfTwo.setAttribute('class', 'column-half')
+
+  const $h4 = document.createElement('h4')
+  $h4.textContent += entry.title
+
+  const $p = document.createElement('p')
+  $p.textContent += entry.notes
+
+$li.append($divHalfOne, $divHalfTwo)
+$divHalfOne.append($img)
+$divHalfTwo.append($h4, $p)
+
+
+return $li
+
+}
+
+// pushes data into list
+const $ul = document.querySelector('ul')
+
+document.addEventListener('DOMContentLoaded', function (event){
+  for (let i = 0; i < data.entries.length; i++){
+  const $finalDOM = renderEntry(data.entries[i])
+  $ul.appendChild($finalDOM)
+  }
+})
+
+const $noEntries = document.querySelector('.p1')
+
+function toggleNoEntrires(){
+  if (data.entries.length > 0){
+$noEntries.setAttribute('class', 'no-entries p1')
+}
+}
+
+
+const $entryForm = document.querySelector("[data-view='entry-form']")
+const $entries = document.querySelector("[data-view='entries']")
+
+function viewSwap (string){
+  data.view = string
+ if (string === "entries") {
+  $entryForm.setAttribute('class', 'hidden')
+  $entries.setAttribute('class', '')
+ } else if (string === "entry-form"){
+  $entries.setAttribute('class', 'hidden')
+  $entryForm.setAttribute('class', '')
+ }
+}
+
+
+const $a = document.querySelector('a')
+
+$a.addEventListener('click', function(event){
+  viewSwap('entries')
+})
